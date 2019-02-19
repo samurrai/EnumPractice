07c49d9fc305d4fa1a00b1bc1188a6e3febab489
samurrai <46028603+samurrai@users.noreply.github.com>
2019-02-19 13:54:37 +0600

    Update Employee.cs

--- Employee.cs
+++ Employee.cs
@@ -6,16 +6,11 @@ using System.Threading.Tasks;
 
 namespace EnumPractice
 {
-    public class Employee
+    public struct Employee
     {
         public string Name { get; set; }
         public Vacancies Vacancy { get; set; }
         public int Salary { get; set; }
         public int[] EmploymentDate { get; set; }
-
-        public Employee()
-        {
-            EmploymentDate = new int[3];
-        }
     }
 }
